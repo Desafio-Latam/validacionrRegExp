6cf57e93569d50d5ddc8a712c0fe432b92196758
kofrann <kofran.cl@gmail.com>
2022-02-15 21:53:01 -0300

    SE agregan funcionalidades

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 //[a-zA-Z]: Encuentra cualquier carácter entre los rangos de la letra a hasta la z y desde A hasta
 //Z. 
-function probarEntrada(regexp, cadena){
+/* function probarEntrada(regexp, cadena){
     var subcadena;
     if (regexp.test(cadena)) {
       subcadena = ' contiene ';
@@ -9,4 +9,19 @@ function probarEntrada(regexp, cadena){
       subcadena = ' no contiene ';
     }
     console.log(cadena + subcadena + regexp.source);
-  }
+  } */
+
+  /* var amigos = ["Erick", "Cristian", "Max", "Claudia"];
+ 
+  console.log(amigos.sort()) */
+
+ /*  var pacientes = [
+    { nombre: 'Vladimir', edad: 22, peso: 55, altura: 173},
+    { nombre: 'Anastacia', edad: 25, peso: 67, altura: 181},
+    { nombre: 'Alexander', edad: 56, peso: 55, altura: 155},
+    
+  ]
+  var sumatoria = pacientes.reduce(function(acumulador, p){
+      return acumulador + p.edad;
+  },  100);
+  console.log(sumatoria) */
